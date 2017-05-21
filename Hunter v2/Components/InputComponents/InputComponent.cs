cc57dfe1b065af1d0311fb321768e5d16e66b0bb
TRushton95 <tom.rushton@hotmail.co.uk>
2017-05-21 01:16:37 +0100

    Made movement smoother
    Changed InputComponent to watch for keys being released as well

--- Hunter v2/Components/InputComponents/InputComponent.cs
+++ Hunter v2/Components/InputComponents/InputComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Hunter_v2.Commands;
+using Hunter_v2.Components.InputComponents.Commands;
 using Hunter_v2.Components.Interfaces;
 using Microsoft.Xna.Framework.Input;
 using System;
@@ -17,43 +18,69 @@ namespace Hunter_v2.Components.InputComponents
         public MouseState currentMouseState { get; set; }
         public MouseState previousMouseState { get; set; }
 
-        public ICommand keyW { get; set; }
-        public ICommand keyA { get; set; }
-        public ICommand keyS { get; set; }
-        public ICommand keyD { get; set; }
-        public ICommand mouseLeft { get; set; }
-        public ICommand nullCommand { get; set; }
-        public List<ICommand> commands { get; set; }
+        public Command keyDownW { get; set; }
+        public Command keyDownA { get; set; }
+        public Command keyDownS { get; set; }
+        public Command keyDownD { get; set; }
+        public Command keyUpW { get; set; }
+        public Command keyUpA { get; set; }
+        public Command keyUpS { get; set; }
+        public Command keyUpD { get; set; }
+        public Command mouseLeft { get; set; }
+        public Command nullCommand { get; set; }
+        public List<Command> commands { get; set; }
 
         public InputComponent()
         {
-            keyW = new MoveUpCommand();
-            keyA = new MoveLeftCommand();
-            keyS = new MoveDownCommand();
-            keyD = new MoveRightCommand();
+            keyDownW = new MoveUpCommand();
+            keyDownA = new MoveLeftCommand();
+            keyDownS = new MoveDownCommand();
+            keyDownD = new MoveRightCommand();
+            keyUpW = new StopVerticalCommand();
+            keyUpA = new StopHorizontalCommand();
+            keyUpS = new StopVerticalCommand();
+            keyUpD = new StopHorizontalCommand();
             mouseLeft = new FireCommand();
             nullCommand = new NullCommand();
         }
 
-        public List<ICommand> processInput()
+        public List<Command> processInput()
         {
-            commands = new List<ICommand>();
+            commands = new List<Command>();
 
             previousKeyboardState = currentKeyboardState;
             currentKeyboardState = Keyboard.GetState();
 
-            //needs velocity instead, position is limited to one direction at a time (see movement commands)
+            //if any keys have been pressed
             if (currentKeyboardState.IsKeyDown(Keys.W)) {
-                commands.Add(keyW);
+                commands.Add(keyDownW);
             }
             if (currentKeyboardState.IsKeyDown(Keys.A)) {
-                commands.Add(keyA);
+                commands.Add(keyDownA);
             }
             if (currentKeyboardState.IsKeyDown(Keys.S)) {
-                commands.Add(keyS);
+                commands.Add(keyDownS);
             }
             if (currentKeyboardState.IsKeyDown(Keys.D)) {
-                commands.Add(keyD);
+                commands.Add(keyDownD);
+            }
+
+            //if any keys have been released
+            if (currentKeyboardState.IsKeyUp(Keys.W) && previousKeyboardState.IsKeyDown(Keys.W))
+            {
+                commands.Add(keyUpW);
+            }
+            if (currentKeyboardState.IsKeyUp(Keys.A) && previousKeyboardState.IsKeyDown(Keys.A))
+            {
+                commands.Add(keyUpA);
+            }
+            if (currentKeyboardState.IsKeyUp(Keys.S) && previousKeyboardState.IsKeyDown(Keys.S))
+            {
+                commands.Add(keyUpS);
+            }
+            if (currentKeyboardState.IsKeyUp(Keys.D) && previousKeyboardState.IsKeyDown(Keys.D))
+            {
+                commands.Add(keyUpD);
             }
 
             previousMouseState = currentMouseState;
